ca4003f6d6d2fed8cc998889c3b23b41aaa6f087
Diego Gonzalez <diego.gonzalez2@alumnos.frgp.utn.edu.ar>
2024-07-28 11:59:37 -0300

    primer commit

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+  <h3>
+  Counter: {{counter}}
+</h3>
+<button (click)="add1(+1)">+1</button>
+<button (click)="resetCounter()">Reset</button>
+<button (click)="add1(-1)"> -1 </button>
+
+
+  `
+})
+
+export class CounterComponent  {
+  public counter:number=10;
+  constructor() { }
+  add1(value:number):void{
+    this.counter +=value;
+  }
+  resetCounter():void{
+    this.counter=10;
+  }
+
+}
